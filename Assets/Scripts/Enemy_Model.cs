86db64b3d019e8879fe033785e37337d304a8047
xingchenxu <l>
2021-09-22 14:05:55 +0800

    第一次提交

---
+++ Assets/Scripts/Enemy_Model.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Model : MonoBehaviour
+{
+
+    public EnemyType EnemyType;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
